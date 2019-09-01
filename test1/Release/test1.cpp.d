15b8e496bb145f7d041433145afed309be0a5032
lichen <1010043642@qq.com>
2019-09-01 20:27:53 +0800

    火焰页面完善

--- test1/Release/test1.cpp.d
+++ test1/Release/test1.cpp.d
@@ -136,6 +136,8 @@ C:\Users\李琛\AppData\Local\Temp\VMBuilds\test1\esp8266_generic\Release\test1.cp
  C:\Users\李琛\Documents\Arduino\libraries\SimpleDHT/SimpleDHT.h \
  C:\Users\李琛\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.5.2\libraries\ESP8266WebServer\src/ESP8266WebServer.h \
  C:\Users\李琛\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.5.2\libraries\ESP8266WebServer\src/detail/RequestHandler.h \
+ C:\Users\李琛\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson.h \
+ C:\Users\李琛\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson.hpp \
  C:\Users\李琛\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.5.2\libraries\Ticker/Ticker.h \
  C:\Users\李琛\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.5.2\cores\esp8266/Schedule.h \
  C:\Users\李琛\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.5.2\cores\esp8266/FS.h
--- test1/test1.ino
+++ test1/test1.ino
@@ -1,14 +1,22 @@
 #include <ESP8266WiFi.h>
 #include <SimpleDHT.h>
 #include <ESP8266WebServer.h>
+#include <ArduinoJson.h>
 #include "Ticker.h"
 #include "FS.h"
 #define QUEUE_LENGTH 30
 #define MAX_SRV_CLIENTS 5 //最大同时连接数
 
 //发送数据缓存
+StaticJsonDocument<200> doc;
 int bufLen = 0;
 byte sbuf[QUEUE_LENGTH * 2];
+
+//火焰传感器 D5
+int pinFire = 14;
+
+//HC-SR501红外传感使用D7
+int pinHCSR = 13;
 
 //DHT11温湿度传感器使用D4接口
 int pinDHT11 = 2;
@@ -29,6 +37,7 @@ const char *password = "rsijpkvs";
 
 bool isLedTurnOn = false;
 bool isReadData = false;
+bool isFire = false;
 String Led_Content = "已关闭";
 
 //温湿度数据
@@ -37,153 +46,12 @@ byte humQueue[QUEUE_LENGTH];
 
 //定时调度
 Ticker myTicker;
+Ticker ticker2;
 
 //网页
 String ledReport = String("OFF");
 String temReport = String("0");
 String humReport = String("0");
-
-String mainPage1 = String("") +
-	"<html5>" +
-	"    <head>" +
-	"        <meta charset = \"utf-8\"/>" +
-	"        <title>ESP8266局域网通信系统</title>" +
-	"        <style type=\"text/css\">" +
-	"            .qui-page{" +
-	"                position: absolute;" +
-	"                left: 200px;" +
-	"                width: 640px;" +
-	"                -webkit-transition: -webkit-transform 0.4s;" +
-	"                transition: transform 0.4s" +
-	"            }" +
-	"            .qui-header{" +
-	"                width: 100%;" +
-	"                height: 44px;" +
-	"                overflow:hidden;" +
-	"                background-color: #2bab79" +
-	"            }" +
-	"            .qui-header a {" +
-	"                display: block;" +
-	"                padding-left: 15px;" +
-	"                font-size: 18px;" +
-	"                text-align: center;" +
-	"                line-height: 44px;" +
-	"                color: white" +
-	"            }" +
-	"            .container{" +
-	"                width: 100%;" +
-	"                -webkit-box-sizing: border-box;" +
-	"                -moz-box-sizing: border-box;" +
-	"                box-sizing: border-box;" +
-	"                margin-top: 50px;" +
-	"                background-color: aliceblue" +
-	"            }" +
-	"            .container p{" +
-	"                text-align: center;" +
-	"                font-size: 24px;" +
-	"            }" +
-	"            .container a {" +
-	"                text - align: center;" +
-    "                font - size: 24px;" +
-    "                background - color: darkgrey" +
-	"			 }" +
-	"            .container canvas{" +
-	"                margin-left: 70px;" +
-    "                width: 500px;" +
-    "                height: 300px;" +
-    "                border: 1px solid black" +
-	"			 }" +
-	"            .qui-asides{" +
-	"                position: center;" +
-	"                left: -200px;" +
-	"                top: 0;" +
-	"                width: 200px" +
-	"            }" +
-	"            .qui-aside {" +
-	"                -webkit-transition: -webkit-transform 0.4s;" +
-	"                transition: transform 0.4s;" +
-	"                /*-webkit-overflow-scrolling: touch;*/" +
-	"                /*overflow-scrolling: touch;*/" +
-	"                position: fixed;" +
-	"                top: 0;" +
-	"                width: 200px;" +
-	"                bottom: 0;" +
-	"                overflow-y: scroll;" +
-	"                background-color: #2d3741" +
-	"            }" +
-	"            .qui-asideHead {" +
-	"                padding: 13px 10px 10px;" +
-	"            }" +
-	"            .qui-asideNav{" +
-	"            }" +
-	"            .qui-asideNav li {" +
-	"                border-top: 1px solid #232d34;" +
-	"                background-color: #36424b" +
-	"            }" +
-	"            .qui-asideNav li:nth-child(even) {" +
-	"                background-color: #364049" +
-	"            }" +
-	"            .qui-asideNav a {" +
-	"                display: block;" +
-	"                padding-left: 15px;" +
-	"                font-size: 16px;" +
-	"                line-height: 44px;" +
-	"                color: #ced1d5" +
-	"            }" +
-	"            .qui-asideTool {" +
-	"                border-top: 9px solid #232d34;" +
-	"                background-color: #2d3741" +
-	"            }" +
-	"            .qui-asideTool li{" +
-	"                border-top: 1px solid #232d34;" +
-	"                background-color: #364049" +
-	"            }" +
-	"            .qui-asideTool a {" +
-	"                display: block;" +
-	"                padding-left: 15px;" +
-	"                font-size: 16px;" +
-	"                line-height: 44px;" +
-	"                color: #ced1d5" +
-	"            }" +
-	"        </style>" +
-	"    </head>" +
-	"    <body>" +
-	"        <section class=\"qui-page\">" +
-	"            <!--header-->" +
-	"            <header class=\"qui-header\">" +
-	"                <a>ESP8266局域网通信系统</a>" +
-	"            </header>" +
-	"            <!--header end-->" +
-	"            <!--begin-->" +
-	"            <section class=\"container\">";
-String mainPage2 = String("") +
-	"            </section>" +
-	"            <!--end-->" +
-	"        </section>" +
-	"        <!--侧边栏-->" +
-	"        <aside class=\"qui-asides\">" +
-	"            <section class=\"qui-aside\">" +
-	"                <section class=\"qui-asideHead\"></section>" +
-	"                <nav class=\"qui-asideNav\">" +
-	"                    <ul>" +
-	"                        <li><a href=\"/\" data-bn-ipg=\"mindex-left-nav-index\"><span>首页</span></a></li>" +
-	"                        <li><a href=\"LED\"><span>LED灯</span></a></li>" +
-	"                        <li><a href=\"TemHum\"><span>温湿数据</span></a></li>" +
-	"                        <li><a href=\"/\"><span>烟霾数据</span></a></li>" +
-	"                        <li><a href=\"/\"><span>火焰指数</span></a></li>" +
-	"                    </ul>" +
-	"                </nav>" +
-	"                <section class=\"qui-asideTool\">" +
-	"                    <ul>" +
-	"                        <li><a href=\"/\"><span>问题反馈</span></a></li>" +
-	"                        <li><a href=\"/\"><span>关于我们</span></a></li>" +
-	"                    </ul>" +
-	"                </section>" +
-	"            </section>" +
-	"        </aside>" +
-	"        <!--侧边栏-->" +
-	"    </body>" +
-	"</html5>";
 
 String getContentType(String filename)
 {
@@ -203,25 +71,8 @@ String getContentType(String filename)
 	return "text/plain";
 }
 
-String RewritePage(String postPage) //组合html
-{
-	String page = mainPage1 + postPage + mainPage2;
-	return page;
-}
-
 void handleRoot()//访问主页
 {
-	/*byte tem = 0;
-	byte hum = 0;
-	dht11.read(&tem, &hum, NULL);
-	temReport = String(tem);
-	humReport = String(hum);
-	String postPage = String("") +
-		"                <p>LED: " + ledReport + "</p>" +
-		"                <p>温度: " + temReport + "℃</p>" +
-		"                <p>湿度: " + humReport + "%</p>";
-	String homePage = RewritePage(postPage);
-	webServer.send(200, "text/html", homePage);*/
 	File file = SPIFFS.open("/index.html", "r");
 	if (!file) {
 		Serial.println("File open failed");
@@ -231,13 +82,8 @@ void handleRoot()//访问主页
 	Serial.println("用户访问了主页");
 }
 
-void handleLED() 
+void handleLED() //访问LED页面
 {
-	/*String postPage = String("") +
-		"                <p>LED" + Led_Content + "</p>" +
-		"                <p><a href = \"Switch\">开/关</a></p>";
-	String ledPage = RewritePage(postPage);
-	webServer.send(200, "text/html", ledPage);*/
 	File file = SPIFFS.open("/LED.html", "r");
 	if (!file) {
 		Serial.println("File open failed");
@@ -247,7 +93,7 @@ void handleLED()
 	Serial.println("用户访问了LED页面");
 }
 
-void handleHomedata() 
+void handleHomedata() //发送主页数据
 {
 	String ledStr, thStr, content;
 	if (isLedTurnOn == false) 
@@ -263,7 +109,7 @@ void handleHomedata()
 	webServer.send(200, "text/html", content);
 }
 
-void handleLeddata()
+void handleLeddata() //发送LED数据
 {
 	String led;
 	if (isLedTurnOn == false)
@@ -277,7 +123,7 @@ void handleLeddata()
 	webServer.send(200, "text/html", led);
 }
 
-void handleWenshidata() 
+void handleWenshidata() //发送温湿度数据
 {
 	String wen, shi, content;
 	wen = String(temQueue[QUEUE_LENGTH - 1]) + "℃";
@@ -286,17 +132,17 @@ void handleWenshidata()
 	webServer.send(200, "text/html", content);
 }
 
-void handleTem() 
+void handleTem() //发送温度
 {
 	webServer.send(200, "text/html", String(temQueue[QUEUE_LENGTH - 1]));
 }
 
-void handleHum() 
+void handleHum() //发送湿度
 {
 	webServer.send(200, "text/html", String(humQueue[QUEUE_LENGTH - 1]));
 }
 
-void handleSwitch()
+void handleSwitch() //开关灯
 {
 	if (isLedTurnOn == false)
 	{
@@ -316,54 +162,8 @@ void handleSwitch()
 	}
 }
 
-void handleTemHum() 
+void handleTemHum() //访问温湿数据页面
 {
-	//byte tem = 0;
-	//byte hum = 0;
-	//dht11.read(&tem, &hum, NULL);
-	//temReport = String(tem);
-	//humReport = String(hum);
-	//String postPage1 = String("") +
-	//	"                <p>温度:" + temReport + "℃&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp湿度:" + humReport + "%</p>" +
-	//	"                <p>温度历史曲线</p>" +
-	//	"                <canvas id=\"TemGraph\"></canvas>" +
-	//	"                <p>湿度历史曲线</p>" +
-	//	"                <canvas id=\"HumGraph\"></canvas>" +
-	//	"                <script type=\"text/javascript\">" +
-	//	"                    var canvas1 = document.getElementById(\'TemGraph\');" +
-	//	"                    var ctx1 = canvas1.getContext(\'2d\');" +
-	//	"                    ctx1.lineWidth = \"1px\";" +
-	//	"                    ctx1.strokeStyle = \"#999\";" +
-	//	"                    ctx1.moveTo(10, 25);" +
-	//	"                    ctx1.lineTo(10, 125);" +
-	//	"                    ctx1.lineTo(280, 125);";
-	//String postPage2 = String("") +
-	//	"                    ctx1.stroke();" +
-	//	"                    var canvas2 = document.getElementById(\'HumGraph\');" +
-	//	"                    var ctx2 = canvas2.getContext(\'2d\');" +
-	//	"                    ctx2.lineWidth = \"1px\";" +
-	//	"                    ctx2.strokeStyle = \"#999\";" +
-	//	"                    ctx2.moveTo(10, 25);" +
-	//	"                    ctx2.lineTo(10, 125);" +
-	//	"                    ctx2.lineTo(280, 125);";
-	//String postPage3 = String("") +
-	//	"                    ctx2.stroke();" +
-	//	"                </script>";
-	//String postPage = String("");
-	////绘制温度曲线
-	//String graph1 = String("") + "                    ctx1.moveTo(10, " + (String)(125 - temQueue[0]) + ");";
-	//for (int i = 1; i < QUEUE_LENGTH - 1; i++) 
-	//{
-	//	graph1 += "                    ctx1.lineTo(" + (String)(10 + 9 * i) + ", " + (String)(125 - temQueue[i]) + ");";
-	//}
-	////绘制湿度曲线
-	//String graph2 = String("") + "                    ctx2.moveTo(" + (String)(10) + ", " + (String)(125 - humQueue[0]) + ");";
-	//for (int i = 1; i < QUEUE_LENGTH - 1; i++) {
-	//	graph2 += "                    ctx2.lineTo(" + (String)(10 + 9 * i) + ", " + (String)(125 - humQueue[i]) + ");";
-	//}
-	//postPage = postPage1 + graph1 + postPage2 + graph2 + postPage3;
-	//String temHumPage = RewritePage(postPage);
-	//webServer.send(200, "text/html", temHumPage);
 	File file = SPIFFS.open("/TemHum.html", "r");
 	if (!file) {
 		Serial.println("File open failed");
@@ -373,32 +173,35 @@ void handleTemHum()
 	Serial.println("用户访问了温湿数据页面");
 }
 
-void getTH(){
+//获取当前温湿度
+void getTH() {
 	byte tem = 0;
 	byte hum = 0;
-  dht11.read(&tem, &hum, NULL);
-  sbuf[0] = tem;
-  sbuf[1] = hum;
-  bufLen = 2; 
+	dht11.read(&tem, &hum, NULL);
+
+	doc["CTemperature"] = String(tem);
+	doc["CHumitiy"] = String(hum);
 }
 
-String getTHhistory(){
-  uint8_t i = 0;
-  String temReport = "temperature: ";
-  for (i = 1; i < QUEUE_LENGTH - 1; i++){
-	  temReport += String(temQueue[i]) + ",";
-    //sbuf[i] = temQueue[i];
-  }
-  temReport += "humidity: ";
-  for(; i < QUEUE_LENGTH * 2; i++){
-    temReport += String(humQueue[i - QUEUE_LENGTH]) + ",";
-    //sbuf[i] = temQueue[i - QUEUE_LENGTH];
-  }
-  return temReport;
-  //bufLen = QUEUE_LENGTH * 2;
+//获取历史温湿度
+void getTHhistory() {
+	uint8_t i;
+
+	JsonArray Htemperature;
+	JsonArray Hhumitiy;
+
+	for (i = 0; i < QUEUE_LENGTH; i++) {
+		Htemperature.add(temQueue[i]);
+	}
+	for (i = 0; i < QUEUE_LENGTH; i++) {
+		Hhumitiy.add(humQueue[i]);
+	}
+
+	doc.add(Htemperature);
+	doc.add(Hhumitiy);
 }
 
-void handleSmoke() 
+void handleSmoke() //访问烟霾数据页面
 {
 	File file = SPIFFS.open("/Smoke.html", "r");
 	if (!file) {
@@ -409,7 +212,7 @@ void handleSmoke()
 	Serial.println("用户访问了烟霾数据页面");
 }
 
-void handleFire()
+void handleFire() //访问火焰指数页面
 {
 	File file = SPIFFS.open("/Fire.html", "r");
 	if (!file) {
@@ -418,6 +221,17 @@ void handleFire()
 	size_t sent = webServer.streamFile(file, "text/html");
 	file.close();
 	Serial.println("用户访问了火焰指数页面");
+}
+
+void handleFiredata() //发送火焰指数
+{
+	if (isFire) {
+		webServer.send(200, "text/html", "危险");
+	}
+	else
+	{
+		webServer.send(200, "text/html", "正常");
+	}
 }
 
 void handleNotFound()
@@ -452,7 +266,7 @@ void handleNotFound()
 	Serial.println("用户访问了一个不存在的网页");
 }
 
-void readData() 
+void readData() //实时读取数据
 {
 	isReadData = true;
 }
@@ -472,6 +286,23 @@ void queuePush(byte newdata, byte queue[])
 		queue[i] = queue[i + 1];
 	}
 	queue[QUEUE_LENGTH - 1] = newdata;
+}
+
+void fireDetect() //火焰监测
+{
+	int x = analogRead(A0);
+	Serial.printf("Fire index: %d.\n", x);
+	if (x >= 550)
+	{
+		digitalWrite(8, HIGH);
+		isFire = true;
+	}
+	else
+	{
+		digitalWrite(8, LOW);
+		isFire = false;
+	}
+
 }
 
 void setup() {
@@ -496,12 +327,12 @@ void setup() {
 		delay(500);
 		Serial.print(".");
 	}
-  Serial.println(" ");
+	Serial.println(" ");
 	Serial.println("Connected");
 
-  tcpServer.begin();
-  tcpServer.setNoDelay(true);
-  Serial.println("TCP Server started, IP Address: " + WiFi.localIP().toString() + "  Port: 8266");
+	tcpServer.begin();
+	tcpServer.setNoDelay(true);
+	Serial.println("TCP Server started, IP Address: " + WiFi.localIP().toString() + "  Port: 8266");
 
 	webServer.begin();
 	Serial.printf("Web server started, open %s in a web browser\n", WiFi.localIP().toString().c_str());
@@ -538,12 +369,14 @@ void setup() {
 	webServer.on("/getHum", handleHum);
 	webServer.on("/Smoke", handleSmoke);
 	webServer.on("/Fire", handleFire);
+	webServer.on("/firedata", handleFiredata);
 	webServer.onNotFound(handleNotFound);
 	webServer.begin();
 	Serial.println("HTTP server started");
 
 	//定时调度
 	myTicker.attach(1, readData);
+	ticker2.attach(2, fireDetect);
 }
 
 void loop() {
@@ -592,18 +425,24 @@ void loop() {
 		  else if (quest == "1" && isLedTurnOn == false) {
 			  handleSwitch();
 		  }
-          else if(quest == "50"){
-            getTH();
-            serverClients[i].write(sbuf, bufLen);
-            Serial.printf("TCP client %d get current TH data. \n", i);
+          else if(quest == "50"){ //请求当前温湿度
+			  getTH();
+			  serializeJson(doc, serverClients[i]);
+			  Serial.printf("TCP client %d get current TH data： \n", i);
+			  serializeJson(doc, Serial);
+			  Serial.println();
+			  Serial.println();
+			  doc.clear();
           }
-          else if(quest == "3"){
-            String report = getTHhistory();
-            for(int j = 0; j < report.length(); j++){
-               serverClients[i].write(byte(report[j]));
-            }
-            Serial.printf("TCP client %d get previous TH data. \n", i);            
-          }
+		  else if (quest == "51") { //请求历史温湿度
+			  getTHhistory();
+			  serializeJson(doc, serverClients[i]);
+			  Serial.printf("TCP client %d get history TH data： \n", i);
+			  serializeJson(doc, Serial);
+			  Serial.println();
+			  Serial.println();
+			  doc.clear();
+		  }
         }
       }
     }
